c884d70530e2b1083ee38a3d258045537dccaaa0
Dell <Dell@ZISHAN>
2021-10-19 16:44:36 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace UDException
+{
+    //Working on User Defined Exception 
+    class InvalidAgeException: Exception
+    {
+        public InvalidAgeException(string message) : base(message)
+        {
+
+        }
+    }
+    class Program
+    {
+        public static void Validate(int Age)
+        {
+            if (Age < 18)
+            {
+                throw new InvalidAgeException("Your Age is Not Valid");
+            }
+
+        }        
+        static void Main(string[] args)
+        {
+            try
+            {
+                Validate(17);
+            }
+            catch(InvalidAgeException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
